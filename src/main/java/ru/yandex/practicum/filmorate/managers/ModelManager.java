617c5e978b08e94804906966b90b6777c6339e56
Danila <dannysladkiy@yandex.ru>
2023-04-26 14:28:29 +0300

    228

--- src/main/java/ru/yandex/practicum/filmorate/managers/ModelManager.java
+++ src/main/java/ru/yandex/practicum/filmorate/managers/ModelManager.java
@@ -7,9 +7,14 @@ import java.util.Optional;
 
 public interface ModelManager<T extends Model> {
     T add(T object);
+
     Optional<T> find(int id);
+
     boolean remove(int id);
+
     void clear();
+
     ArrayList<T> getAll();
+
     T change(T model);
 }
