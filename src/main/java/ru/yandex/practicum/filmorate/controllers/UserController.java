c6f1d7e70b8fea7853aee79721545c7c7aaee2b4
Danila <dannysladkiy@yandex.ru>
2023-04-26 14:17:10 +0300

    228

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -12,28 +12,25 @@ import java.time.LocalDate;
 @RestController
 @Slf4j
 @RequestMapping("/api/users")
-public class UserController extends Controller<User>{
-    private final LocalDate CURRENT_DATE = LocalDate.now();
-    public UserController(){
+public class UserController extends Controller<User> {
+    private static final LocalDate CURRENT_DATE = LocalDate.now();
+    public UserController() {
         manager = Managers.userManager;
     }
+
     @Override
     public void validate(User object) throws ValidationException {
-        if(object.getEmail().equals("")){
+        if (object.getEmail().equals("")) {
             throw new ValidationException("Электронная почта не может быть пустой", object);
-        }
-        else if(!object.getEmail().contains("@")){
+        } else if (!object.getEmail().contains("@")) {
             throw new ValidationException("Электронная почта должна содержать символ @", object);
-        }
-        else if(object.getLogin().equals("")){
+        } else if (object.getLogin().equals("")) {
             throw new ValidationException("Логин не может быть пустым", object);
-        }
-        else if(object.getLogin().contains(" ")){
+        } else if (object.getLogin().contains(" ")) {
             throw new ValidationException("Логин не может содержать пробелы", object);
-        }
-        else if(object.getBirthday().isAfter(CURRENT_DATE)){
+        } else if (object.getBirthday().isAfter(CURRENT_DATE)) {
             throw new ValidationException("Дата рождения не может быть в будущем", object);
         }
-        }
     }
+}
 
