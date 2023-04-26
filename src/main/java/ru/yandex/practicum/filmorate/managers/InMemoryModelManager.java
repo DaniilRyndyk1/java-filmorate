975602b3b6cde7336ddfa783ca0fafa22cb155ba
Danila <dannysladkiy@yandex.ru>
2023-04-26 14:23:36 +0300

    228

--- src/main/java/ru/yandex/practicum/filmorate/managers/InMemoryModelManager.java
+++ src/main/java/ru/yandex/practicum/filmorate/managers/InMemoryModelManager.java
@@ -6,45 +6,50 @@ import ru.yandex.practicum.filmorate.models.Model;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
+
 @Slf4j
-public class InMemoryModelManager<T extends Model> implements ModelManager<T>{
+public class InMemoryModelManager<T extends Model> implements ModelManager<T> {
     private final HashMap<Integer, T> objects = new HashMap<>();
     private Integer id = 1;
     @Override
-    public T add(T object){
+    public T add(T object) {
         object.setId(id);
         objects.put(id, object);
-        log.info("Успешно был добавлен объект типа "+ object.getClass().getSimpleName() + " с id = " + id);
+        log.info("Успешно был добавлен объект типа " + object.getClass().getSimpleName() + " с id = " + id);
         id++;
         return object;
     }
+
     @Override
-    public Optional<T> find(int id){
+    public Optional<T> find(int id) {
         return Optional.ofNullable(objects.get(id));
     }
+
     @Override
-    public boolean remove(int id){
-        if (objects.containsKey(id)){
+    public boolean remove(int id) {
+        if (objects.containsKey(id)) {
             objects.remove(id);
             return true;
         }
         return false;
     }
+
     @Override
-    public void clear(){
+    public void clear() {
         objects.clear();
     }
+
     @Override
-    public ArrayList<T> getAll(){
+    public ArrayList<T> getAll() {
         ArrayList<T> result = new ArrayList<>();
-        for (int id : objects.keySet()){
+        for (int id : objects.keySet()) {
             var value = objects.get(id);
             result.add(value);
         }
         return result;
     }
     @Override
-    public T change(T model){
+    public T change(T model) {
         int id = model.getId();
         remove(id);
         objects.put(id, model);
