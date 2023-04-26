c8f755d628aaa0396088202f67d48096b6265cdc
Danila <dannysladkiy@yandex.ru>
2023-04-26 14:18:26 +0300

    228

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @RequestMapping("/api/users")
 public class UserController extends Controller<User> {
     private static final LocalDate CURRENT_DATE = LocalDate.now();
+
     public UserController() {
         manager = Managers.userManager;
     }
